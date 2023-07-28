869e671b41c28b82a1c2db46a925ae9284940494
Mihyun Jun <jjunmmee@gmail.com>
2023-07-28 17:02:59 +0900

    .

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,13 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.urls import reverse
 from django.utils import timezone
+from taggit.managers import TaggableManager
+
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
 
 
 # Create your models here.
@@ -10,7 +17,7 @@ class Post(models.Model):
         PUBLISHED = 'PB', 'Published'
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish', allow_unicode=True)
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
@@ -25,6 +32,10 @@ class Post(models.Model):
         choices=Status.choices,
         default=Status.DRAFT
     )
+    tags = TaggableManager()
+
+    objects = models.Manager()
+    published = PublishedManager()
 
     class Meta:
         ordering = ['-publish']
@@ -34,3 +45,28 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.publish.year,
+                                                 self.publish.month,
+                                                 self.publish.day,
+                                                 self.slug])
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ['created']
+        indexes = [
+            models.Index(fields=['created'])
+        ]
+
+    def __str__(self):
+        return f'Comment by {self.name} on {self.post}'
--- mysite/settings.py
+++ mysite/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!4ktl+m1hxq%wb4+0&_x^r2g#%y@5$%2j9)5o@e606-zr42s+*'
+SECRET_KEY = 'django-insecure-nj*xwg%ab##j6tt^&*-id10w(&-!ujli#v$4jdu*no(1(ned8h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
